187912f03a046c51d917b615e734e82b245f3c89
Brett <gumpyland@gmail.com>
2017-09-19 11:33:25 -0600

    Initial commit

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessesAllowed = 10;
+
+	public Text text;
+
+	public Text guesses;
+
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame ()
+	{
+		max = 1000;
+		min = 1;
+		NextGuess();
+
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+
+	void NextGuess ()
+	{
+		guess = Random.Range(min,max+1);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		guesses.text = "Guesses Remaining: " + maxGuessesAllowed.ToString();
+		if(maxGuessesAllowed <= 0){
+		SceneManager.LoadScene("Win");
+		}
+	}
+}
---
+++ Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame ()
+	{
+		max = 1000000;
+		min = 1;
+		guess = 500000;
+
+
+		print ("========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me!");
+
+		print ("Pick a number between " + min + "and " + max + "!");
+
+		print ("Is the number higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+
+		max = max + 1;
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			//print ("Up arrow pressed");
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			//print ("Down arrow pressed");
+			max = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
+			StartGame();
+		}
+
+	}
+
+	void NextGuess ()
+	{
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+	}
+}
