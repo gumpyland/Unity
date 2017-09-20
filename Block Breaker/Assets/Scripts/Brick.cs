9f2dd7a77ebc5fadf49d30baf23e37800dfe2a87
Brett <gumpyland@gmail.com>
2017-09-19 22:42:17 -0600

    Damaged Bricks

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+	
+	public Sprite[] hitSprites;
 
 	private int timesHit;
-
 	private LevelManager levelManager;
 
 	// Use this for initialization
@@ -21,14 +21,28 @@ public class Brick : MonoBehaviour {
 		
 	}
 
-	void OnCollisionEnter2D (Collision2D col) {
+	void HandleHits () {
 		timesHit++;
+		int maxHits = hitSprites.Length + 1;
 		if (timesHit == maxHits) {
 			Destroy (gameObject);
+		} else {
+			LoadSprites();
 		}
 	}
-	// TODO Remove this method once we can actually win!
-	void SimulateWin () {
-		levelManager.LoadNextLevel ();
+
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]){
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
 	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			HandleHits();
+		}
+	}
+
 }
